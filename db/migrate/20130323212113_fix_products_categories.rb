b37926b465482f6bf10046bd5afcc830d6a4fc7c
novohispano@gmail.com <novohispano@gmail.com>
2013-03-23 16:03:10 -0600

    Categories showing

---
+++ db/migrate/20130323212113_fix_products_categories.rb
@@ -0,0 +1,5 @@
+class FixProductsCategories < ActiveRecord::Migration
+  def change
+    add_column :products, :category_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130323203917) do
+ActiveRecord::Schema.define(:version => 20130323212113) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(:version => 20130323203917) do
     t.boolean  "featured"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.integer  "category_id"
   end
 
 end
