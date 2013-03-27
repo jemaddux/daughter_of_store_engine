01ddf2f054486e8c34634facfcf4906616da4836
Kyle Suss <susskyle@gmail.com>
2013-03-26 18:45:11 -0600

    added migration for paperclip

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,7 @@ class Product < ActiveRecord::Base
                   :quantity,
                   :featured,
                   :category_id
+                  :image
 
   validates :name,        presence: true, uniqueness: true
   validates :description, presence: true
@@ -12,4 +13,6 @@ class Product < ActiveRecord::Base
   validates :quantity,    presence: true
   # validates :category_id, presence: true
   belongs_to :category
+
+  has_attached_file :image, :styles => { :medium => "454x627>", :thumb => "182x304>" }, :default_url => "http://placehold.it/1000x1000&text=Thumbnail"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130323212113) do
+ActiveRecord::Schema.define(:version => 20130327002859) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -26,9 +26,13 @@ ActiveRecord::Schema.define(:version => 20130323212113) do
     t.integer  "price"
     t.integer  "quantity"
     t.boolean  "featured"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
     t.integer  "category_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
