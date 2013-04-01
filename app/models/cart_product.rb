09d781d126da7faf978a9fc72bf9a02890cfd2f5
Christopher <ChrisKnight.mail@gmail.com>
2013-03-31 18:34:18 -0600

    made min quantity in cart 1

--- app/models/cart_product.rb
+++ app/models/cart_product.rb
@@ -2,6 +2,12 @@ class CartProduct < ActiveRecord::Base
   attr_accessible :quantity,
                   :price
 
+  validates :quantity_is_one, presence: true
+
+  def quantity_is_one
+    Product.find(product_id).quantity >= 1
+  end
+
   belongs_to :cart
   belongs_to :product
 end
