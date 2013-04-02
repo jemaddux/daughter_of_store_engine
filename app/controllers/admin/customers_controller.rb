38e8f5de77007ce6bccd4a854cf1beafb977876d
Christopher <ChrisKnight.mail@gmail.com>
2013-04-02 13:57:47 -0600

    UI functions and admin customers page

---
+++ app/controllers/admin/customers_controller.rb
@@ -0,0 +1,59 @@
+class Admin::CustomersController < ApplicationController 
+
+  layout 'admin/application.html.haml'
+
+  before_filter :require_admin
+
+  def index
+    @customers = Customer.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @cusomters }
+    end
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @customer }
+    end
+  end
+
+  def update
+    @order = Order.find(params[:id])
+
+    respond_to do |format|
+      if @order.update_attributes(params[:order])
+        format.html { redirect_to @order, notice: 'Order was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @order.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @order = Order.find(params[:id])
+    @order.destroy
+
+    respond_to do |format|
+      format.html { redirect_to admin_orders_path }
+      format.json { head :no_content }
+    end
+  end
+
+
+  private
+ 
+  def require_admin
+    if logged_in?
+      redirect_to login_url unless current_user.admin
+    else 
+      redirect_to login_url
+    end
+  end
+end
