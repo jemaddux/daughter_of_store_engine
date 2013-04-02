7966b6262ce1c2ba37c2d97a082494d208ae5349
novohispano@gmail.com <novohispano@gmail.com>
2013-04-02 17:42:55 -0600

    Deleting the index customer route from routes

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,8 +1,4 @@
 class CustomersController < ApplicationController
-  def index
-    @customers = Customer.all
-  end
-
   def show
     @customer = Customer.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 StoreEngine::Application.routes.draw do
-  resources :orders,            only: [:show, :index]
-  resources :carts,             only: [:show, :update, :destroy]
-  resources :categories,        only: [:show, :index]
-  resources :products,          only: [:show, :index]
-  resources :customers
-  resources :customer_sessions, only: [:new, :create, :destroy]
-  resources :charges,           only: [:new, :create]
-  resources :cart_products,     only: [:destroy]
+  resources :orders,            only:   [:show, :index]
+  resources :carts,             only:   [:show, :update, :destroy]
+  resources :categories,        only:   [:show, :index]
+  resources :products,          only:   [:show, :index]
+  resources :customers,         except: [:index]
+  resources :customer_sessions, only:   [:new, :create, :destroy]
+  resources :charges,           only:   [:new, :create]
+  resources :cart_products,     only:   [:destroy]
 
   match 'login'  => 'customer_sessions#new'
   match 'logout' => 'customer_sessions#destroy'
