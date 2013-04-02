a43555a6cc3ab74d125eca8769d6d6038ed53cdc
novohispano@gmail.com <novohispano@gmail.com>
2013-04-02 17:04:37 -0600

    Cleaning it up

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,29 +1,14 @@
 class CustomersController < ApplicationController
   def index
     @customers = Customer.all
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @customers }
-    end
   end
 
   def show
     @customer = Customer.find(params[:id])
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @customer }
-    end
   end
 
   def new
     @customer = Customer.new
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @customer }
-    end
   end
 
   def edit
@@ -33,30 +18,21 @@ class CustomersController < ApplicationController
   def create
     @customer = Customer.new(params[:customer])
 
-    respond_to do |format|
-      if @customer.save
-        Mailer.welcome_email(@customer).deliver
-
-        format.html { redirect_to @customer, notice: 'Customer was successfully created.' }
-        format.json { render json: @customer, status: :created, location: @customer }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @customer.errors, status: :unprocessable_entity }
-      end
+    if @customer.save
+      Mailer.welcome_email(@customer).deliver
+      redirect_to @customer, notice: 'Customer was successfully created.'
+    else
+      render action: "new"
     end
   end
 
   def update
     @customer = Customer.find(params[:id])
 
-    respond_to do |format|
-      if @customer.update_attributes(params[:customer])
-        format.html { redirect_to @customer, notice: 'Customer was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @customer.errors, status: :unprocessable_entity }
-      end
+    if @customer.update_attributes(params[:customer])
+      redirect_to @customer, notice: 'Customer was successfully updated.'
+    else
+      render action: "edit"
     end
   end
 
@@ -64,9 +40,6 @@ class CustomersController < ApplicationController
     @customer = Customer.find(params[:id])
     @customer.destroy
 
-    respond_to do |format|
-      format.html { redirect_to customers_url }
-      format.json { head :no_content }
-    end
+    redirect_to customers_url
   end
 end
