6381b90b73598d6690fd5727d67dc899271975fa
novohispano@gmail.com <novohispano@gmail.com>
2013-04-04 01:36:26 -0600

    Merging changes

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -7,4 +7,28 @@ class ShippingAddress < ActiveRecord::Base
                   :customer_id
 
   belongs_to :customer
+
+  validates :street,  presence: true
+  validates :city,    presence: true
+
+  validates :state,   presence: true, length: { 
+    minimum: 2,
+    maximum: 2,
+    too_short: "Enter a 2 character state code",
+    too_long: "Enter a 2 character state code"     
+  }
+
+  validates :zipcode, presence: true, { 
+    minimum: 5,
+    maximum: 5,
+    too_short: "Enter a 5 digit zipcode",
+    too_long:  "Enter a 5 digit zipcode" 
+  }
+
+  validates :phone, presence: true, { 
+    minimum: 10,
+    maximum: 10,
+    too_short: "Enter a 10 digit phone number",
+    too_long:  "Enter a 10 digit phone number" 
+  }
 end
