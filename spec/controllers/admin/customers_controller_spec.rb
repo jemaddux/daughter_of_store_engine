0835485d9c9b4b35dba31f53236b1dc583f4b20d
novohispano@gmail.com <novohispano@gmail.com>
2013-04-03 21:29:59 -0600

    Testing Admin Customer controller completed

--- spec/controllers/admin/customers_controller_spec.rb
+++ spec/controllers/admin/customers_controller_spec.rb
@@ -17,41 +17,19 @@ describe Admin::CustomersController do
     { user_id: 1, order_id: 1 }
   end
 
-  describe "PUT update" do
-    describe "with valid params" do
-      it "updates the requested customer" do
-        customer = Customer.create! valid_attributes
-        put :update, {id: 1, :customer => { "first_name" => "Kylie" }}, valid_session
-        expect( customer.first_name ).to be "Kylie"
-      end
-
-      it "assigns the requested customer as @customer" do
-        customer = Customer.create! valid_attributes
-        put :update, {:id => customer.to_param, :customer => valid_attributes}, valid_session
-        assigns(:customer).should eq(customer)
-      end
-
-      it "redirects to the customer" do
-        customer = Customer.create! valid_attributes
-        put :update, {:id => customer.to_param, :customer => valid_attributes}, valid_session
-        response.should redirect_to(admin_customers_path)
-      end
+  describe "GET index" do
+    it "assigns all products as @products" do
+      customer = Customer.create! valid_attributes
+      get :index, {}, valid_session
+      assigns(:customers).should eq([customer])
     end
+  end
 
-    describe "with invalid params" do
-      it "assigns the customer as @customer" do
-        customer = Customer.create! valid_attributes
-        customer.any_instance.stub(:save).and_return(false)
-        put :update, {:id => customer.to_param, :customer => {  }}, valid_session
-        assigns(:customer).should eq(customer)
-      end
-
-      it "re-renders the 'edit' template" do
-        customer = Customer.create! valid_attributes
-        customer.any_instance.stub(:save).and_return(false)
-        put :update, {:id => customer.to_param, :customer => {  }}, valid_session
-        response.should render_template("edit")
-      end
+  describe "GET show" do
+    it "assigns the requested product as @product" do
+      customer = Customer.create! valid_attributes
+      get :show, {:id => customer.to_param}, valid_session
+      assigns(:customer).should eq(customer)
     end
   end
 
@@ -59,14 +37,14 @@ describe Admin::CustomersController do
     it "destroys the requested customer" do
       customer = Customer.create! valid_attributes
       expect {
-        delete :destroy, {:id => customer.to_param}, valid_session
-      }.to change(customer, :count).by(-1)
+        delete :destroy, { id: customer.to_param }, valid_session
+      }.to change(Customer, :count).by(-1)
     end
 
     it "redirects to the customers list" do
       customer = Customer.create! valid_attributes
-      delete :destroy, {:id => customer.to_param}, valid_session
-      expect(response).to redirect_to(customers_path)
+      delete :destroy, { id: customer.to_param }, valid_session
+      expect(response).to redirect_to(admin_customers_path)
     end
   end
 end
