de54aef18dd79823970e268b99c9707449e0d3bc
novohispano@gmail.com <novohispano@gmail.com>
2013-04-03 23:21:10 -0600

    Testing the cart

--- spec/features/admin/category_spec.rb
+++ spec/features/admin/category_spec.rb
@@ -8,28 +8,23 @@ describe "category" do
     login_customer_post("admin", "admin")
   end
 
-  # it "should create a category" do 
-  #   visit new_admin_category_path
-  #   fill_in "category_name",        :with => "New category"
-  #   fill_in "category_description", :with => "Some description"
-  #   fill_in "category_quantity",    :with => 7
-  #   fill_in "category_price",       :with => 1234
-  #   choose "category_featured_false"
-  #   choose "category_active_true"
-  #   click_button "Create category"
-  #   expect( page ).to have_content "Some description"
-  #   expect( page ).to have_content "1,234"
-  # end
+  let(:category) { Category.create(name: "Name", description: "Description") }
 
-  # it "edits the individual category" do 
-  #   visit edit_admin_category_path(@category)
-  #   fill_in "category_name",        :with => "Some other category"
-  #   fill_in "category_description", :with => "Some other description"
-  #   fill_in "category_price",       :with => 4321
-  #   fill_in "category_quantity",    :with => 77
-  #   choose "category_featured_false"
-  #   click_button "Update Category"
-  #   expect( page ).to have_content "Some other description"
-  #   expect( page ).to have_content "4,321"
-  # end
+  it "should create a category" do 
+    visit new_admin_category_path
+    fill_in "category_name",        :with => "New category"
+    fill_in "category_description", :with => "Some description"
+    click_button "Create Category"
+    expect( page ).to have_content "New category"
+    expect( page ).to have_content "Some description"
+  end
+
+  it "edits the individual category" do 
+    visit edit_admin_category_path(category)
+    fill_in "category_name",        :with => "Some other category"
+    fill_in "category_description", :with => "Some other description"
+    click_button "Update Category"
+    expect( page ).to have_content "Some other category"
+    expect( page ).to have_content "Some other description"
+  end
 end
--- spec/features/categories_spec.rb
+++ spec/features/categories_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe "categories" do
-
   before(:each) do
     Category.create(name: "Some category", description: "Cat desc").save
     @category = Category.find_by_name("Some category")
@@ -16,42 +15,4 @@ describe "categories" do
     visit category_path(@category)
     expect( page ).to have_content "Some category"
   end
-
-  # context "when given all the correct parameters" do
-  #   it "should create a category" do 
-  #     visit new_admin_category_path
-  #     fill_in "category_name",        :with => "Test category"
-  #     fill_in "category_description", :with => "Test description"
-  #     click_button "Create Category"
-  #     expect( page ).to have_content "Test category"
-  #     expect( page ).to have_content "Test description"
-  #   end
-
-  #   it "should edit a category" do
-  #     visit edit_admin_category_path(@category)
-  #     fill_in "category_name", :with => "Test update"
-  #     fill_in "category_description", :with => "Test description update"
-  #     click_button "Update Category"
-  #     expect( page ).to have_content "Test update"
-  #     expect( page ).to have_content "Test description update"
-  #   end
-  # end
-
-  # context "when given incorrect parameters" do
-  #   it "should not create a category" do
-  #     visit new_admin_category_path
-  #     fill_in "category_name",        :with => "Test category"
-  #     fill_in "category_description", :with => ""
-  #     click_button "Create Category"
-  #     expect( page ).to have_css('.field_with_errors')
-  #   end
-
-  #   it "should not edit a category" do
-  #     visit edit_admin_category_path(@category)
-  #     fill_in "category_name", :with => "Test update"
-  #     fill_in "category_description", :with => ""
-  #     click_button "Update Category"
-  #     expect( page ).to have_css('.field_with_errors')
-  #   end
-  # end
 end
