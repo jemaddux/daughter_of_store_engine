48baad05c46d1d00e3efdd2b67fbad598c53bb96
Logan Sears <lsears322@gmail.com>
2013-04-17 15:48:19 -0600

    Products now have valid price formatting for their prices. Prices are stored as bigdecimals in the database. Forms updated to reflect this.

--- db/migrate/20130328054725_change_integer_to_decimal_in_product_price.rb
+++
@@ -1,5 +0,0 @@
-class ChangeIntegerToDecimalInProductPrice < ActiveRecord::Migration
-  def change
-    change_column :products, :price, :decimal
-  end
-end
--- db/migrate/20130414211356_add_display_name_to_customers.rb
+++ db/migrate/20130414211356_add_display_name_to_customers.rb
@@ -1,5 +1,5 @@
 class AddDisplayNameToCustomers < ActiveRecord::Migration
   def change
-    add_column :customers, :display_name, :string
+    add_column :customers, :display_name, :string, :precision => 8, :scale => 2
   end
 end
