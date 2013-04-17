d135e6b58b59aae086f252f6a31d7904d209572c
Christopher <ChrisKnight.mail@gmail.com>
2013-04-16 20:42:33 -0600

    customer account creation story done / called customer not user though

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,10 +7,10 @@ class Customer < ActiveRecord::Base
                             message: "Password should match confirmation.",
                             if: :password
 
-  validates :username,   uniqueness: true
-  validates :email,      presence: true, uniqueness: true
-  validates :first_name, presence: true
-  validates :last_name,  presence: true
+  validates :display_name,  presence: true
+  validates :email,         presence: true, uniqueness: true
+  validates :first_name,    presence: true
+  validates :last_name,     presence: true
 
   has_one  :cart
   has_one  :shipping_address
