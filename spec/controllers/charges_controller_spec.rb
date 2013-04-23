d1150f3f4c4ba69da44177746269f4c1181f9d3d
Paul <pnblackwell@gmail.com>
2013-04-23 16:49:23 -0600

    fixed spacing

---
+++ spec/controllers/charges_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe ChargesController do
+
+# let!(:user) {Customer.create!(email: 'test@test.com', password: 'password', first_name: 'test', last_name: 'user', admin: false)}
+# let!(:store) {Store.create!(name: 'Cool Sunglasses', path: 'cool-sunglasses', description: 'We have cool sunglasses', status: 'active')}
+# # let!(:shopping_cart) {Cart.create! ({1=>{2=>2, 3=>4}})}
+  
+#   context 'when you create a new charge with an authenticated user' do
+#     it 'has at least one cart_product' do
+#       visit login_path
+#       visit new_charge_path(store)
+#       page.should have_content('Order Total:')
+#     end
+#   end
+
+  # context 'When creating a new charge' do
+    
+  # end
+end
