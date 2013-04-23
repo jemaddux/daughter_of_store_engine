780ae904a76a0ad675b2ee34c779b1bd38ef48d7
John Maddux <jemaddux@gmail.com>
2013-04-23 10:43:15 -0600

    Admin can now administrate stores

--- app/controllers/store_admin/stores_controller.rb
+++ app/controllers/store_admin/stores_controller.rb
@@ -1,7 +1,7 @@
 class StoreAdmin::StoresController < ApplicationController
   layout 'admin/application'
   skip_filter :scope_current_store
-  before_filter :require_store_admin
+  before_filter :require_store_admin_or_admin
 
   #def index
   #  @store = Store.all
