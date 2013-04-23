7d9539a849d463456c74b31e4f194e9858a6ec32
Phil Battos <philbattos@gmail.com>
2013-04-23 17:48:09 -0600

    added tests for store statuses

--- spec/features/store_spec.rb
+++ spec/features/store_spec.rb
@@ -1,10 +1,5 @@
 require 'spec_helper'
 
-# test that stores' homepage shows appropriate status of stores
-# Given that a store has been created but not yet approved by site admin
-# When a user types in the url of that store
-# Then the store's page shows a message saying that the store is pending
-
 describe Store do
   let!(:user) {Customer.create!(email: 'test@test.com', password: 'password', first_name: 'test', last_name: 'user', admin: false)}
 
@@ -38,19 +33,18 @@ describe Store do
   end
 
   context 'when the site admin rejects a store' do
-    it 'displays a message telling the store creator that the store has been rejected' do
-      visit stores_path
-      click_link 'New Store'
-      fill_in 'email', with: 'test@test.com'
-      fill_in 'password', with: 'password'
-      click_button 'Login'
-      fill_in 'store_name', with: 'carrot sticks'
-      fill_in 'store_path', with: 'carrot-sticks'
-      fill_in 'store_description', with: 'we sell carrots and sticks'
-      click_button 'Save'
-      # site admin rejects store
-      visit '/carrot-sticks/admin'
-      page.should have_content('Status: declined')
+    let!(:store) {Store.create!(name: 'Cool Sunglasses', path: 'cool-sunglasses', description: 'We have cool sunglasses', status: 'declined')}
+    it 'displays a message that the store has been rejected' do
+      visit '/cool-sunglasses'
+      page.should have_content('This store has been rejected')
+    end
+  end
+
+  context 'when a site admin disables a store' do
+    let!(:store) {Store.create!(name: 'Cool Sunglasses', path: 'cool-sunglasses', description: 'We have cool sunglasses', status: 'disabled')}
+    it 'should display a message that the store is down for maintenance' do
+      visit '/cool-sunglasses'
+      page.should have_content('This store is down for maintenance')
     end
   end
 end
