374baa45922e4fb21ba36053f6ac4186c2407196
John Maddux <jemaddux@gmail.com>
2013-04-28 11:54:51 -0600

    Edited file for bullet gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,11 +40,15 @@ StoreEngine::Application.configure do
   config.after_initialize do
     Bullet.enable = true
     Bullet.alert = true
-    Bullet.bullet_logger = false
-    Bullet.console = false
-    Bullet.growl = false
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    # Bullet.growl = true
+    # Bullet.xmpp = { :account  => 'bullets_account@jabber.org',
+    #                 :password => 'bullets_password_for_jabber',
+    #                 :receiver => 'your_account@jabber.org',
+    #                 :show_online_status => true }
     Bullet.rails_logger = true
-    Bullet.airbrake = false
+    # Bullet.airbrake = true
   end
 
 
