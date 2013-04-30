11a13f15363b939115579c16861e630986a3883d
Blair Anderson <blair81@gmail.com>
2013-04-30 13:41:43 -0600

    recompiled assets

--- app/assets/javascripts/ace.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-var editor = ace.edit("editor");
-editor.setTheme("ace/theme/twilight");
-
-$("#textarea_ID").hide();
-$("#form_ID").submit(function(){
-$("#textarea_ID").val(editor.getSession().getValue());
-});
-});
