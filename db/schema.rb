fbcc9217fd34ac1b1906e27e8c62bfb8117f4e5e
Blair Anderson <blair81@gmail.com>
2013-04-29 19:54:46 -0600

    schema stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130428211138) do
+ActiveRecord::Schema.define(:version => 20130429204644) do
 
   create_table "addresses", :force => true do |t|
     t.integer  "status"
@@ -49,6 +49,18 @@ ActiveRecord::Schema.define(:version => 20130428211138) do
   end
 
   add_index "background_images", ["store_id"], :name => "index_background_images_on_store_id"
+
+  create_table "cart_products", :force => true do |t|
+    t.integer  "cart_id"
+    t.integer  "product_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.decimal  "price"
+    t.integer  "quantity"
+  end
+
+  add_index "cart_products", ["cart_id"], :name => "index_cart_products_on_cart_id"
+  add_index "cart_products", ["product_id"], :name => "index_cart_products_on_product_id"
 
   create_table "carts", :force => true do |t|
     t.integer  "customer_id"
@@ -130,6 +142,7 @@ ActiveRecord::Schema.define(:version => 20130428211138) do
     t.integer  "store_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "slug"
   end
 
   create_table "product_categories", :force => true do |t|
@@ -205,9 +218,9 @@ ActiveRecord::Schema.define(:version => 20130428211138) do
     t.string   "button_color_hover"
     t.string   "background_color"
     t.string   "text_color"
+    t.string   "layout",             :default => "default"
     t.string   "text_font"
     t.string   "custom_css"
-    t.string   "layout",             :default => "default"
   end
 
 end
