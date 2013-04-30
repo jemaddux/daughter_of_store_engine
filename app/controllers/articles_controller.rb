069c7b18da36b79766df248dfaa1f314ba6991d2
Paul <pnblackwell@gmail.com>
2013-04-30 14:11:10 -0600

    finished the dropdown functionality w/ styling

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,50 @@
+class ArticlesController < ApplicationController
+  before_filter :require_store_admin_or_admin, except: [:show, :index]
+
+  def index
+    @articles = current_store.articles
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    article = Article.new(store_id:current_store.id, customer_id: current_user.id)
+    article.update_attributes(params[:article])
+    if article.save
+      redirect_to article_path(current_store, article)
+    else
+      render :new
+    end
+  end
+
+  def update
+    article = Article.find(params[:id])
+    article.update_attributes(params[:article])
+    if article.save
+      redirect_to article_path(current_store, article)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+    @article.increase_view_count
+  end
+
+  def increase_view_count
+    
+  end
+
+  def destroy
+    article = Article.find(params[:id])
+    article.destroy
+    redirect_to store_admin_path(current_store) 
+  end
+end
