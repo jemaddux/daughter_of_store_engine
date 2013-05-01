6d2d27b2c935312cbde69a732ab23aaea6c5c094
Blair Anderson <blair81@gmail.com>
2013-05-01 16:33:01 -0600

    seed stuff

--- app/jobs/build_seed_products.rb
+++ app/jobs/build_seed_products.rb
@@ -3,14 +3,16 @@ class BuildSeedProducts
 
   def self.perform(store_id)
     1000.times do |i|
-      name = Faker::Name.name
-      description = Faker::Lorem.paragraph(2)
-      Product.create!(store_id: store_id, name: name, description: description,
-                      price: "#{(1..500).to_a.sample}.0".to_f,
-                      quantity: "#{(1..500).to_a.sample}".to_i,
-                      featured: false, active: true)
-      print "."
+      cats = %w(sunglasses goggles skis snowboards boots helmets gloves backpacks cameras luggage clothing)
+      Product.create!(
+        store_id: store_id,
+        categories_list:cats.sample,
+        name: Faker::Name.name, 
+        description: Faker::Lorem.paragraph(4),
+        price: "#{(1..500).to_a.sample}.0".to_f,
+        quantity: "#{(1..500).to_a.sample}".to_i,
+        featured: false, 
+        active: true)
     end
   end
-
 end
